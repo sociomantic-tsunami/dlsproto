988be5d850d8b827d1a1e06c11fb40dc6c7bab92
Nemanja Boric <nemanja.boric@sociomantic.com>
2017-12-19 12:19:10 +0100

    Flush in client after sending Continue/Stop

--- src/dlsproto/client/request/internal/GetRange.d
+++ src/dlsproto/client/request/internal/GetRange.d
@@ -551,6 +551,8 @@ private scope class GetRangeHandler
                         payload.addConstant(MessageType_v1.Continue);
                     }
                 );
+
+                this.outer.conn.flush();
             }
 
             this.outer.request_event_dispatcher.signal(this.outer.conn,
@@ -814,6 +816,7 @@ private scope class GetRangeHandler
                                 payload.addConstant(MessageType_v1.Stop);
                             }
                         );
+                        this.outer.conn.flush();
                         this.outer.context.shared_working.stopped = true;
                         break;
 
