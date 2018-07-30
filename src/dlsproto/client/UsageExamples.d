1559b5d618c49c69c72d7a34df14464f8c0b23dc
Gavin Norman <gavin.norman@sociomantic.com>
2018-07-30 16:56:41 +0200

    Update copyright owner

--- src/dlsproto/client/UsageExamples.d
+++ src/dlsproto/client/UsageExamples.d
@@ -3,7 +3,7 @@
     Usage examples for the Neo DLS Client.
 
     Copyright:
-        Copyright (c) 2017 sociomantic labs GmbH. All rights reserved.
+        Copyright (c) 2017 dunnhumby Germany GmbH. All rights reserved.
 
     License:
         Boost Software License Version 1.0. See LICENSE.txt for details.
